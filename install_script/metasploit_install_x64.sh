d4a09fec5cb5bc04b9f7bb9b23ee279b3e7c656e
hakawati <hakawati@hakawati-VirtualBox.(none)>
2014-08-02 18:23:39 +0900

    Metasploit install

--- install_script/metasploit_install_x64.sh
+++ install_script/metasploit_install_x64.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y ruby1.9.1 build-essential
 wget http://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run
 sudo chmod +x metasploit-latest-linux-x64-installer.run
 sudo ./metasploit-latest-linux-x64-installer.run
-rm metasploit-lastest-linux-x64-installer.run
+rm metasploit-latest-linux-x64-installer.run
