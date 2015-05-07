e75a400c75ee1245e8bc902ccf89a98cd5460c54
hakawati <hakawati@naver.com>
2015-05-07 16:36:47 +0900

    add vsftpd

--- install_script/apm_install.sh
+++ install_script/apm_install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install -y apache2 php5 mysql-server mysql-client php5-mysql phpmyadmin libmysqlclient15-dev
+sudo apt-get install -y apache2 php5 mysql-server mysql-client php5-mysql phpmyadmin libmysqlclient15-dev vsftpd
